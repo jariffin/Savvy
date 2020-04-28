7680d34fc00bbaa3c77af295011d3b29a3633837
Penina Cheshin <penk85@gmail.com>
2020-04-28 15:57:23 +0200

    results page

--- app/controllers/garments_controller.rb
+++ app/controllers/garments_controller.rb
@@ -1,15 +1,14 @@
 class GarmentsController < ApplicationController
 
   def new
-
+    @garment = Garment.new
   end
 
   def create
-
   end
 
   def show
-
+    @garment = Garment.find(params[:id])
   end
 
   def index
