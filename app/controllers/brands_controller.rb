55de4766bd6d0b742d43a7718c3b6660dc1a77d9
Juliana Ariffin <juliana@fastmail.com>
2020-05-01 10:11:57 +0200

    user can upload images

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -7,15 +7,11 @@ class BrandsController < ApplicationController
   def create
     @brand = Brand.new(brands_params)
     @garment = Garment.find(params:[:garment_id])
-    # can use garment instance here, before garment is saved?
     @brand.garment_id = @garment.id
     @brand.save
   end
 
-
   private
-
-  # possible with just name?
 
   def brands_params
     params.require(:brand).permit(:name, :website, :email)
