0f01acce721a7fb5e9dcd3cc4030cd5f26bd79e2
Patricia Pellicer <ppellicer@gmx.ch>
2020-05-04 18:01:27 +0200

    basic static page, information to be added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'score_information', to: 'pages#score_information'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :garments, only: [ :new, :create, :show, :index ] do
