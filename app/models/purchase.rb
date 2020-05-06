7b91754412ce3f30cdb458c13add997374e44f93
Patricia Pellicer <ppellicer@gmx.ch>
2020-05-06 12:36:06 +0200

    changes uploading picture, grid reverse, font issues

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -4,11 +4,11 @@ class Purchase < ApplicationRecord
 
   has_one_attached :image
 
-  def default_image
-    if purchase.garment.image.attached? == false
-      "https://images.unsplash.com/photo-1527519124254-9dafd8b3533b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1534&q=80"
+  def purchase_image
+    if self.garment.image.attached?
+      self.garment.image.key
     else
-      purchase.garment.image.key
+      "https://images.unsplash.com/photo-1558361716-f8144bad90f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1234&q=80"
     end
   end
 end
