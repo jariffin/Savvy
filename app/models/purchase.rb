7bf58e0384ed2a4cb4a6460026fe8baf21501dbc
Patricia Pellicer <ppellicer@gmx.ch>
2020-05-06 08:37:53 +0200

    to be tested

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -3,5 +3,13 @@ class Purchase < ApplicationRecord
   belongs_to :user
 
   has_one_attached :image
+
+  def default_image
+    if purchase.garment.image.attached?
+      "https://images.unsplash.com/photo-1527519124254-9dafd8b3533b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1534&q=80"
+    else
+      purchase.garment.image.key
+    end
+  end
 end
 
