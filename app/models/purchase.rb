b45a0f12ec164e49e95af7d5e9c5756b92916387
Patricia Pellicer <ppellicer@gmx.ch>
2020-05-06 10:15:37 +0200

    default in progress

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -5,7 +5,7 @@ class Purchase < ApplicationRecord
   has_one_attached :image
 
   def default_image
-    if purchase.garment.image.attached?
+    if purchase.garment.image.attached? == false
       "https://images.unsplash.com/photo-1527519124254-9dafd8b3533b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1534&q=80"
     else
       purchase.garment.image.key
